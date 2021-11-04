159a9549fd6352e95d388968dcfe42fa93f1e85b
Attila Radl <attila.radl@cern.ch>
2021-11-04 16:33:06 +0100

    Finalization of the AlCaPCCRandom PCL worfklow

--- Calibration/LumiAlCaRecoProducers/python/ALCARECOAlCaPCCRandom_cff.py
+++ Calibration/LumiAlCaRecoProducers/python/ALCARECOAlCaPCCRandom_cff.py
@@ -1,11 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 
+import HLTrigger.HLTfilters.hltHighLevel_cfi
+ALCARECORandomHLT = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+    HLTPaths = cms.vstring("*Random*"),
+    eventSetupPathsKey='',
+    TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
+    andOr = cms.bool(True), # choose logical OR between Triggerbits
+    throw = cms.bool(False) # tolerate triggers stated above, but not available
+)
+
 from Calibration.LumiAlCaRecoProducers.alcaPCCIntegrator_cfi import alcaPCCIntegrator
 alcaPCCProducerRandom = alcaPCCIntegrator.clone()
 alcaPCCProducerRandom.inputPccLabel = cms.string("hltAlcaPixelClusterCounts")
-alcaPCCProducerRandom.trigstring    = cms.untracked.string("alcaPCCEvent")
+alcaPCCProducerRandom.trigstring    = cms.untracked.string("alcaPCCRandom")
 alcaPCCProducerRandom.ProdInst      = cms.string("alcaPCCRandom")
 
 # Sequence #
-
-seqALCARECOAlCaPCCRandom = cms.Sequence(alcaPCCProducerRandom)
+seqALCARECOAlCaPCCRandom = cms.Sequence(ALCARECORandomHLT+alcaPCCProducerRandom)
